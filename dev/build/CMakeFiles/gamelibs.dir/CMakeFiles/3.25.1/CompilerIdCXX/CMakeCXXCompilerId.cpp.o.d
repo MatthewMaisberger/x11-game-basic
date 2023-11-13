27b244eae5c1f0d7c65479eab3877cc3fa506645
Char <matthew.maisberger@gmail.com>
2023-11-13 15:29:18 -0600

    post gitignore

--- dev/build/CMakeFiles/gamelibs.dir/CMakeFiles/3.25.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/gamelibs.dir/CMakeFiles/3.25.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/char/cpp/x11game/dev/build/CMakeFiles/3.25.1/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
